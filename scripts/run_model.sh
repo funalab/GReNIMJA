09ed79f40bb9b611fef9b06bceb1eb8e8e549643
morikura <morikura@fun.bio.keio.ac.jp>
2025-12-17 11:51:27 +0900

    debug main_sensitivity_analysis.py

--- scripts/run_model.sh
+++ scripts/run_model.sh
@@ -2,33 +2,33 @@
 
 # sensitivity analysis
 # encoding
-python src/main.py --epoch_num 10 --mer 5 --amino_mer 4 --amino_emb_dim 100 --dna_emb_dim 50 --embedding embedding
-python src/main.py --epoch_num 10 --mer 5 --amino_mer 4 --amino_emb_dim 100 --dna_emb_dim 50 --embedding one_hot
+python src/main_sensitivity_analysis.py --epoch_num 10 --mer 5 --amino_mer 4 --amino_emb_dim 100 --dna_emb_dim 50 --embedding embedding
+python src/main_sensitivity_analysis.py --epoch_num 10 --mer 5 --amino_mer 4 --amino_emb_dim 100 --dna_emb_dim 50 --embedding one_hot
 
 # mer
-python src/main.py --epoch_num 10 --mer 1 --amino_mer 4 --amino_emb_dim 100 --dna_emb_dim 50
-python src/main.py --epoch_num 10 --mer 3 --amino_mer 4 --amino_emb_dim 100 --dna_emb_dim 50
-python src/main.py --epoch_num 10 --mer 5 --amino_mer 4 --amino_emb_dim 100 --dna_emb_dim 50
-python src/main.py --epoch_num 10 --mer 8 --amino_mer 4 --amino_emb_dim 100 --dna_emb_dim 50
-python src/main.py --epoch_num 10 --mer 10 --amino_mer 4 --amino_emb_dim 100 --dna_emb_dim 50
+python src/main_sensitivity_analysis.py --epoch_num 10 --mer 1 --amino_mer 4 --amino_emb_dim 100 --dna_emb_dim 50
+python src/main_sensitivity_analysis.py --epoch_num 10 --mer 3 --amino_mer 4 --amino_emb_dim 100 --dna_emb_dim 50
+python src/main_sensitivity_analysis.py --epoch_num 10 --mer 5 --amino_mer 4 --amino_emb_dim 100 --dna_emb_dim 50
+python src/main_sensitivity_analysis.py --epoch_num 10 --mer 8 --amino_mer 4 --amino_emb_dim 100 --dna_emb_dim 50
+python src/main_sensitivity_analysis.py --epoch_num 10 --mer 10 --amino_mer 4 --amino_emb_dim 100 --dna_emb_dim 50
 
 # amino_mer
-python src/main.py --epoch_num 10 --mer 5 --amino_mer 2 --amino_emb_dim 100 --dna_emb_dim 50
-python src/main.py --epoch_num 10 --mer 5 --amino_mer 4 --amino_emb_dim 100 --dna_emb_dim 50
-python src/main.py --epoch_num 10 --mer 5 --amino_mer 6 --amino_emb_dim 100 --dna_emb_dim 50
-python src/main.py --epoch_num 10 --mer 5 --amino_mer 8 --amino_emb_dim 100 --dna_emb_dim 50
-python src/main.py --epoch_num 10 --mer 5 --amino_mer 10 --amino_emb_dim 100 --dna_emb_dim 50
+python src/main_sensitivity_analysis.py --epoch_num 10 --mer 5 --amino_mer 2 --amino_emb_dim 100 --dna_emb_dim 50
+python src/main_sensitivity_analysis.py --epoch_num 10 --mer 5 --amino_mer 4 --amino_emb_dim 100 --dna_emb_dim 50
+python src/main_sensitivity_analysis.py --epoch_num 10 --mer 5 --amino_mer 6 --amino_emb_dim 100 --dna_emb_dim 50
+python src/main_sensitivity_analysis.py --epoch_num 10 --mer 5 --amino_mer 8 --amino_emb_dim 100 --dna_emb_dim 50
+python src/main_sensitivity_analysis.py --epoch_num 10 --mer 5 --amino_mer 10 --amino_emb_dim 100 --dna_emb_dim 50
 
 # amino_emb_dim
-python src/main.py --epoch_num 10 --mer 5 --amino_mer 4 --amino_emb_dim 10 --dna_emb_dim 50
-python src/main.py --epoch_num 10 --mer 5 --amino_mer 4 --amino_emb_dim 50 --dna_emb_dim 50
-python src/main.py --epoch_num 10 --mer 5 --amino_mer 4 --amino_emb_dim 100 --dna_emb_dim 50
-python src/main.py --epoch_num 10 --mer 5 --amino_mer 4 --amino_emb_dim 250 --dna_emb_dim 50
-python src/main.py --epoch_num 10 --mer 5 --amino_mer 4 --amino_emb_dim 500 --dna_emb_dim 50
+python src/main_sensitivity_analysis.py --epoch_num 10 --mer 5 --amino_mer 4 --amino_emb_dim 10 --dna_emb_dim 50
+python src/main_sensitivity_analysis.py --epoch_num 10 --mer 5 --amino_mer 4 --amino_emb_dim 50 --dna_emb_dim 50
+python src/main_sensitivity_analysis.py --epoch_num 10 --mer 5 --amino_mer 4 --amino_emb_dim 100 --dna_emb_dim 50
+python src/main_sensitivity_analysis.py --epoch_num 10 --mer 5 --amino_mer 4 --amino_emb_dim 250 --dna_emb_dim 50
+python src/main_sensitivity_analysis.py --epoch_num 10 --mer 5 --amino_mer 4 --amino_emb_dim 500 --dna_emb_dim 50
 
 # dna_emb_dim
-python src/main.py --epoch_num 10 --mer 5 --amino_mer 4 --amino_emb_dim 100 --dna_emb_dim 10
-python src/main.py --epoch_num 10 --mer 5 --amino_mer 4 --amino_emb_dim 100 --dna_emb_dim 50
-python src/main.py --epoch_num 10 --mer 5 --amino_mer 4 --amino_emb_dim 100 --dna_emb_dim 100
-python src/main.py --epoch_num 10 --mer 5 --amino_mer 4 --amino_emb_dim 100 --dna_emb_dim 250
-python src/main.py --epoch_num 10 --mer 5 --amino_mer 4 --amino_emb_dim 100 --dna_emb_dim 500
+python src/main_sensitivity_analysis.py --epoch_num 10 --mer 5 --amino_mer 4 --amino_emb_dim 100 --dna_emb_dim 10
+python src/main_sensitivity_analysis.py --epoch_num 10 --mer 5 --amino_mer 4 --amino_emb_dim 100 --dna_emb_dim 50
+python src/main_sensitivity_analysis.py --epoch_num 10 --mer 5 --amino_mer 4 --amino_emb_dim 100 --dna_emb_dim 100
+python src/main_sensitivity_analysis.py --epoch_num 10 --mer 5 --amino_mer 4 --amino_emb_dim 100 --dna_emb_dim 250
+python src/main_sensitivity_analysis.py --epoch_num 10 --mer 5 --amino_mer 4 --amino_emb_dim 100 --dna_emb_dim 500
